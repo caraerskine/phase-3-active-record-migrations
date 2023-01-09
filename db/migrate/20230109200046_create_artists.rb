c8e95e72d0946b86f22364364864896c1bd97bcf
caraerskine <104041881+caraerskine@users.noreply.github.com>
2023-01-09 15:20:05 -0500

    commit tree 48f5c7c3185b2cb97ab8e7be82864e671afbd07c

---
+++ db/migrate/20230109200046_create_artists.rb
@@ -0,0 +1,22 @@
+class CreateArtists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+class AddFavoriteFoodToArtists < ActiveRecord::Migration[6.1]
+  def change
+    add_column :artists, :favorite_food, :string
+  end
+end
+
+class AddFavoriteFlowerToArtists < ActiveRecord::Migration[6.1]
+  def change
+    add_column :artists, :favorite_flower, :string
+  end
+end
+end 
